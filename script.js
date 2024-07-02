c89ce81595187f761f8b3b6f5da6d049b4a3e851
clairnet1905 <salamnecesen13@gmail.com>
2024-07-02 10:19:22 +0200

    Update script.js

--- script.js
+++ script.js
@@ -7,16 +7,4 @@ document.querySelectorAll('nav ul li a').forEach(link => {
 
     link.addEventListener('mouseout', () => {
         link.style.transform = 'scale(1)';
-        link.style.transition = 'transform 0.2s';
-    });
-});
-
-// Smooth scroll to sections
-document.querySelectorAll('nav ul li a').forEach(link => {
-    link.addEventListener('click', function(e) {
-        e.preventDefault();
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
-});
+        link
